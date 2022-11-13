3126bdd4869d1b389cb8e2a62ddeae07c600c57c
hyunjun Yang <yhj0439@nate.com>
2022-11-13 13:41:04 +0900

    폴더 구조나누기 & 더미 데이터 확인 완료

---
+++ project/src/App.jsx
@@ -0,0 +1,14 @@
+import HomePage from "./pages/HomPage/HomePage";
+
+import "./reset.css";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
